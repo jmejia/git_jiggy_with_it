25ff723b30a8a13393a381c16d0d8c9608324780
Josh Mejia <joshua.mejia@gmail.com>
2017-03-24 10:09:56 -0600

    Initial commit.

---
+++ lib/bald_eagle.rb
@@ -0,0 +1,2 @@
+class BaldEagle
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+  attr_reader :name, :pets
+
+  def initialize(name = "Mason")
+    @name = name
+    @pets = []
+  end
+
+  def add_pet(pet)
+  end
+end
+
